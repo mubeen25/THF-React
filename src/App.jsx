7d950517539b107537ce5cfe996dfe0efc441f6f
Mohtashim_Wajid <91398268+Mohtashim-Wajid@users.noreply.github.com>
2023-06-12 23:33:33 +0500

    merge conflicts resolved at mushi's end

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,8 @@ import './App.css'
 import Navbar from './Components/Navbar'
 import Carousal from './Components/Carousal'
 import About from './Components/About'
-<<<<<<< HEAD
 import DonersCarousel from './Components/DonersCarousel'
-=======
 import Skills from './Components/Skills'
->>>>>>> d45908cb7d2c206afec1c1723df55524375f92fc
 
 function App() {
 
