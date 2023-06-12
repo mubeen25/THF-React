5eb20a7f1edd7ec452ab799c0213d014308fb99d
mubeen25 <mobinsoomro21@gmail.com>
2023-06-12 22:46:54 +0500

    Resolved Conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,8 @@ import React from 'react'
 import './App.css'
 import Navbar from './Components/Navbar'
 import Carousal from './Components/Carousal'
-<<<<<<< HEAD
-import Skills from './Components/Skills'
-=======
 import About from './Components/About'
->>>>>>> 9bd18dfb56b0bb4a7a5e05173d1ede8137e68a47
+import Skills from './Components/Skills'
 
 function App() {
 
@@ -15,11 +12,8 @@ function App() {
     <>
       <Navbar/>
       <Carousal/>
-<<<<<<< HEAD
       <Skills/>
-=======
       <About/>
->>>>>>> 9bd18dfb56b0bb4a7a5e05173d1ede8137e68a47
     </>
   )
 }
