cb2ba404c30572461c1d8ff92fa3bb7560f280ad
AbdulMoiz001 <78033318+AbdulMoiz001@users.noreply.github.com>
2023-06-15 02:45:48 +0500

    skill animation Changes

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -14,16 +14,18 @@ const Skills = () => {
             (entries) => {
                 entries.forEach((entry) => {
                     if (entry.isIntersecting) {
+                        entry.target.classList.remove('card-before-fade-skills');
                         entry.target.classList.add('fade-in-from-left');
                     } else {
                         entry.target.classList.remove('fade-in-from-left');
+                        entry.target.classList.add('card-before-fade-skills');
                     }
                 });
             },
             { rootMargin: '0px', threshold: 0.2 }
         );
 
-        const animatedElements = document.querySelectorAll('.fade-in-from-left');
+        const animatedElements = document.querySelectorAll('.card-before-fade-skills');
         animatedElements.forEach((element) => {
             observer.current.observe(element);
         });
@@ -37,23 +39,23 @@ const Skills = () => {
         <>
             <section id="Methodologies">
                 <div className="row methods-row">
-                    <div className="col-lg-2 offset-lg-1 fade-in-from-left">
+                    <div className="col-lg-2 offset-lg-1 card-before-fade-skills">
                         <img className="img-fluid methods-icons" src={conventional} alt="conventional" />
                         <h3 className="text-lines">Conventional Training</h3>
                     </div>
-                    <div className="col-lg-2 fade-in-from-left">
+                    <div className="col-lg-2 card-before-fade-skills">
                         <img className="img-fluid methods-icons" src={technology} alt="conventional" />
                         <h3 className="text-lines">High-Tech Training</h3>
                     </div>
-                    <div className="col-lg-2 fade-in-from-left">
+                    <div className="col-lg-2 card-before-fade-skills">
                         <img className="img-fluid methods-icons" src={innovation} alt="conventional" />
                         <h3 className="text-lines">Entrepreneurship Trainings</h3>
                     </div>
-                    <div className="col-lg-2 fade-in-from-left">
+                    <div className="col-lg-2 card-before-fade-skills">
                         <img className="img-fluid methods-icons" src={learning} alt="conventional" />
                         <h3 className="text-lines">On-Job Trainings</h3>
                     </div>
-                    <div className="col-lg-2 fade-in-from-left">
+                    <div className="col-lg-2 card-before-fade-skills">
                         <img className="img-fluid methods-icons" src={ThreeD} alt="conventional" />
                         <h3 className="text-lines">3D Printing Services</h3>
                     </div>
